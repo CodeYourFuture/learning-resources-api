1092e88e2924306f564b65e87afdcab9720d3ef8
Jacobo Koenig <jacobo360@gmail.com>
2020-04-07 16:53:42 +0100

    Debugging

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,14 @@ app.get('/resources', async (req, res) => {
 
     //2. Check API Key Exists
     let apiKeyRequested = req.query.api_key
-    let apiKeySnap = await db.collection('api_keys').doc(apiKeyRequested)
+
+    if (!apiKeyRequested) {
+      return res.status(401).send({
+        'error': 'Please include your API key when making requests',
+      });
+    }
+
+    let apiKeySnap = await db.collection('api_keys').doc(apiKeyRequested).get()
 
     if (!apiKeySnap.exists) {
       return res.status(401).send({
@@ -40,61 +47,93 @@ app.get('/resources', async (req, res) => {
   }
 });
 
+//1. Generate new API keys
+app.post('/api-keys', async (req, res) => {
+  try {
+    let documentRef = db.collection('api_keys').doc()
+
+    //4. Validate all required data is included and accurate
+    let missingParams = []
+    if (!req.body.owner_name) missingParams.push('owner_name')
+
+    if (missingParams.length > 0) {
+      return res.status(400).send({
+        'error': 'Missing Parameters',
+        'missing_params': missingParams,
+      });
+    }
+
+    let objectToSave = {
+      'api_key': documentRef.id,
+      'owner_name': req.body.owner_name,
+      'type': 'test'
+    }
+
+    await documentRef.set(objectToSave);
+    return res.status(201).send({
+      'message': 'API Key Saved',
+      'api_key': documentRef.id,
+      'type': 'test',
+    });
+  } catch (error) {
+    return res.status(401).send({ 'error': `${error}` });
+  }
+});
+
+app.post('/resources', async (req, res) => {
+  try {
+    //2. Check API Key Exists
+    let apiKey = req.query.api_key
+
+    if (!apiKey) {
+      return res.status(401).send({
+        'error': 'Please include your API key when making requests',
+      });
+    }
+
+    console.log(apiKey)
+
+    let apiKeySnap = await db.collection('api_keys').doc(apiKey).get()
+
+    if (!apiKeySnap.exists) {
+      return res.status(401).send({
+        'error': 'API Key does not exist!',
+      });
+    }
+
+    //4. Validate all required data is included and accurate
+    let missingParams = []
+    if (!req.body.name) missingParams.push('name')
+    if (!req.body.url) missingParams.push('url')
+    if (!req.body.type) missingParams.push('type')
+
+    if (missingParams.length > 0) {
+      return res.status(400).send({
+        'error': 'Missing Parameters',
+        'missing_params': missingParams,
+      });
+    }
+
+    //5. Add other parameters
+    let objectToSave = {
+      'name': req.body.name,
+      'url': req.body.url,
+      'type': req.body.type,
+      'api_key': apiKey,
+    }
+    await db.collection('resources').add(objectToSave);
+    return res.status(201).send({ 'message': 'good job!' });
+  } catch (error) {
+    return res.status(401).send({ 'error': `${error}` });
+  }
+});
+
 app.get('/*', async (req, res) => {
   return res.status(404).send({ 'error': 'Route not found!' });
 });
 
-//1. Generate new API keys
-app.post('/api-keys', async (req, res) => {
-  let documentRef = db.collection('api_keys').doc()
-
-  let objectToSave = {
-    'api_key': documentRef.documentID,
-    'owner_name': req.body.owner_name,
-    'type': 'test'
-  }
-
-  await documentRef.add(objectToSave);
-  return res.status(201).send({
-    'message': 'API Key Saved',
-    'api_key': documentRef.documentID,
-    'type': test,
-  });
-});
-
-app.post('/resources', async (req, res) => {
-  //2. Check API Key Exists
-  let apiKey = req.query.api_key
-  let apiKeySnap = await db.collection('api_keys').doc(apiKeyRequested)
-
-  if (!apiKeySnap.exists) {
-    return res.status(401).send({
-      'error': 'API Key does not exist!',
-    });
-  }
-
-  //4. Validate all required data is included and accurate
-  let missingParams = []
-  if (!req.body.name) missingParams.push('name')
-  if (!req.body.url) missingParams.push('url')
-  if (!req.body.type) missingParams.push('type')
-
-  if (missingParams.length > 0) {
-    return res.status(400).send({
-      'error': 'Missing Parameters',
-      'missing_params': missingParams,
-    });
-  }
-
-  //5. Add other parameters
-  let objectToSave = {
-    'name': req.body.name,
-    'url': req.body.url,
-    'type': req.body.type,
-    'api_key': apiKey,
-  }
-  await db.collection('resources').add(objectToSave);
-  return res.status(201).send({ 'message': 'good job!' });
+app.post('/*', async (req, res) => {
+  return res.status(404).send({ 'error': 'Route not found!' });
 });
 
 exports.api = functions.https.onRequest(main);
